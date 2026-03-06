2d36b220d94ce2263c737329d410f0919bc947d3
davidosick <dave.shavlidze@gmail.com>
2026-03-06 13:34:36 +0300

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
         destination: 'https://app.xdao.app/1/:path*',
         permanent: false,
       },
+      {
+        source: '/:chain/dao/:address',
+        destination: 'https://app.xdao.app/:chain/dao/:address',
+        permanent: false,
+      },
     ]
   },
 }
